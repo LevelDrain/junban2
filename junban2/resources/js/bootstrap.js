063c85086bb77439171b876363ebb142d48ffcb3
LevelSchema <drain2291@gmail.com>
2025-06-14 13:42:38 +0900

    CSRFの追加

--- junban2/resources/js/bootstrap.js
+++ junban2/resources/js/bootstrap.js
@@ -1,4 +1,14 @@
 import axios from 'axios';
 window.axios = axios;
 
+window.axios.defaults.withCredentials = true;
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+axios.get('/sanctum/csrf-cookie')
+  .then(() => {
+      console.log('CSRF cookie set');
+  })
+  .catch((error) => {
+    console.error('CSRF cookie failed', error);
+  });
