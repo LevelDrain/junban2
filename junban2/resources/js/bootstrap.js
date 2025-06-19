3b4eb0c5c74f95c9a7f8c2edce2477f6549586c9
LevelSchema <drain2291@gmail.com>
2025-06-19 21:51:46 +0900

    csrf-cookieの受け渡しができるようにする

--- junban2/resources/js/bootstrap.js
+++ junban2/resources/js/bootstrap.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
+
 window.axios = axios;
 
-window.axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL || '/';
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const token = document.querySelector('meta[name="csrf-token"]');
+if (token) {
+  axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+  console.error('CSRF token not found in meta tag');
+}
 
 axios.get('/sanctum/csrf-cookie')
   .then(() => {
