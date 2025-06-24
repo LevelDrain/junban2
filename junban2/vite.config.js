60727fd74b2a83556b0f2396db90eb7112fc242f
LevelSchema <drain2291@gmail.com>
2025-06-25 01:43:48 +0900

    vite.config.jsの修正に伴うファイル入れ替え

--- junban2/vite.config.js
+++ junban2/vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
